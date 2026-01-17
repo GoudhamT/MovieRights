28fdd1fdc25ea2678a63e970d2fed838b85750cf
GoudhamT <goudhamvel.88@gmail.com>
2026-01-16 18:23:32 -0800

    Bid implemented

--- Makefile
+++ Makefile
@@ -1,10 +1,15 @@
 .PHONY: test compile build install
 
+include .env
+
 install:
 	forge install smartcontractkit/chainlink-brownie-contracts
 	forge remappings > remappings.txt
 	
 compile :; forge compile 
+
 build:
 	forge build 
+
 test :; forge test 
+
--- src/MovieRights.sol
+++ src/MovieRights.sol
@@ -26,7 +26,14 @@ pragma solidity 0.8.19;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract MovieRights {
+    /*Errors */
+    error MovieRights__NotEnoughMoneyforAuction(uint256 _money);
     /* type declarations */
+    enum AuctionStatus {
+        OPEN,
+        ASSIGNING,
+        CLOSED
+    }
     struct AuctionDetails {
         string movieName;
         uint256 minPriceInUSD;
@@ -35,26 +42,46 @@ contract MovieRights {
         address[] highestBiders;
         address[] bidders;
         uint256 highestBidAmount;
+        AuctionStatus auctionStatus;
     }
 
     /*state variables */
     AuctionDetails private s_auctionDetails;
     address private s_owner;
+    AggregatorV3Interface public s_priceFeed;
 
     /*Events */
     event MovieRights__auctionCreated(
         string indexed movieName,
         uint256 minPrice
     );
+    event MovieRights__PlacedBid(
+        string indexed movieName,
+        address indexed bidder,
+        uint256 amount
+    );
 
-    constructor() {
+    constructor(address _feedAddress) {
         s_owner = msg.sender;
+        s_priceFeed = AggregatorV3Interface(_feedAddress);
     }
 
     /*Modifiers */
     modifier OwnerCannotBid() {
         require(msg.sender != s_owner, "You cannot bid");
         _;
+    }
+
+    function _getEthPriceInUSD() internal view returns (uint256) {
+        uint256 minPrice = (s_auctionDetails.minPriceInUSD) * 1e18;
+        uint256 EthInUSD = _getPrice();
+        uint256 calculatedETH = (minPrice * 1e8) / EthInUSD;
+        return calculatedETH;
+    }
+
+    function _getPrice() internal view returns (uint256) {
+        (, int256 price, , , ) = s_priceFeed.latestRoundData();
+        return uint256(price);
     }
 
     /*Functions */
@@ -69,8 +96,31 @@ contract MovieRights {
         auction.minPriceInUSD = _minPrice;
         auction.auctionDuration = _auctionDuration;
         auction.rightsDuration = _rightsDuration;
+        auction.auctionStatus = AuctionStatus.OPEN;
 
         emit MovieRights__auctionCreated(_name, _minPrice);
+    }
+
+    function enterBid() public payable {
+        require(
+            s_auctionDetails.auctionStatus == AuctionStatus.OPEN,
+            "Auction is not Open"
+        );
+
+        if (msg.value < _getEthPriceInUSD()) {
+            revert MovieRights__NotEnoughMoneyforAuction(msg.value);
+        }
+        /*Push Bidder */
+        s_auctionDetails.bidders.push(msg.sender);
+        if (msg.value >= getHighestAmount()) {
+            s_auctionDetails.highestBidAmount = msg.value;
+            s_auctionDetails.highestBiders.push(msg.sender);
+        }
+        emit MovieRights__PlacedBid(
+            s_auctionDetails.movieName,
+            msg.sender,
+            msg.value
+        );
     }
 
     /*view & pure functions */
@@ -84,5 +134,9 @@ contract MovieRights {
 
     function getAuctionName() external view returns (string memory) {
         return s_auctionDetails.movieName;
+    }
+
+    function getHighestAmount() public view returns (uint256) {
+        return s_auctionDetails.highestBidAmount;
     }
 }

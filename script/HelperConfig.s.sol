ad241591cad7beadec27025e63f3119dfcd7dae8
GoudhamT <goudhamvel.88@gmail.com>
2026-01-16 19:11:03 -0800

    mock feed added

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,11 +2,14 @@
 
 pragma solidity 0.8.19;
 import {Script, console} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "@chainlink/contracts/src/v0.8/tests/MockV3Aggregator.sol";
 
 abstract contract ConfigConstants {
     uint256 public constant SEPOLIA_CHAIN_ID = 11155111;
     uint256 public constant MAINNET_CHAIN_ID = 1;
     uint256 public constant LOCAL_ID = 31337;
+    uint8 public constant MOCK_DECIMALS = 8;
+    int256 public constant MOCK_ANSWER = 2000e10;
 }
 
 contract HelperConfig is Script, ConfigConstants {
@@ -37,5 +40,13 @@ contract HelperConfig is Script, ConfigConstants {
             });
     }
 
-    function getLocalConfig() public returns (NetworkConfig memory) {}
+    function getLocalConfig() public returns (NetworkConfig memory) {
+        vm.startBroadcast();
+        MockV3Aggregator mockFeed = new MockV3Aggregator(
+            MOCK_DECIMALS,
+            MOCK_ANSWER
+        );
+        vm.stopBroadcast();
+        return NetworkConfig({priceFeed: address(mockFeed)});
+    }
 }
